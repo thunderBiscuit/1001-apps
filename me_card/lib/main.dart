63fbbde1a88b7e18a0ce858208f87a0caf5ddeeb
thunderbiscuit <thunderB@protonmail.com>
2020-07-13 13:31:31 -0400

    challenge 1 meCard

--- me_card/lib/main.dart
+++ me_card/lib/main.dart
@@ -10,9 +10,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100,
+                color: Colors.red,
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 100,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
