a71093e177074ae3ba8d29e75a6aa215b8d8847c
thunderbiscuit <thunderB@protonmail.com>
2020-07-16 10:30:34 -0400

    basic ui dice app

---
+++ dicee-flutter/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/td/tdhome/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/td/tdhome/repos/1001-apps/dicee-flutter"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/td/tdhome/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
